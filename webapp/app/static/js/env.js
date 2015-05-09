4215f1a6e78190cf97455a61f409e5252cd12062
McGiver- <gkolampas@gmail.com>
2015-05-09 07:50:12 -0400

    js requests

--- webapp/app/static/js/env.js
+++ webapp/app/static/js/env.js
@@ -1,20 +1,35 @@
 var countdown=null;
+var isBrewing = true;
+function updateBrewing(callback){
+	$.get("/api/status",function(response){
+		isBrewing = response.brewing;
+		callback(isBrewing);
+	})
+}
+
 $( document ).ready(function() {
+    
+    updateBrewing(function(isBrewing){
+   	 	$("amount").text("Are we brewing"+response.brewing);
+    });
+   
     var countdownDom = document.getElementById("countdown");
+   
+    $("#doit").click(function(){
+    	$.post("/api/brew", {"brewing":true},function(response){
+        $("#status").text("Brewing...");
+   		});
+	});
 
-	$("#doit").click(function() {
-		var currentNum =document.getElementById("time").value;
-
-		function innerCountdown() {
-			if(currentNum!=-1){
-				countdownDom.innerHTML=currentNum;
-				currentNum--;
-				setTimeout(innerCountdown,1000);
-			}
-			return false;
-		}
-
-		innerCountdown();
-		return false;
+    $("#doit").click(function(){
+   	 	$.get("/api/specs", function(response){
+       		$("#cups").text("Number of Cups of Coffee"+reponse.cups+"Number of mL of fluid Max"+reponse.ml);
+   	 	});
+	});
+    
+    $("#doit").click(function(){
+   		$.get("/api/status", function(response){
+        	$("#amount").text("Are we brewing"+response.brewing);
+   	 	});
 	});
 });
