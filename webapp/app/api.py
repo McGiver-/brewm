42764884c1cc95d6d99628d01a6856b6b0ba007c
Jacob Errington <nictytan@gmail.com>
2015-05-09 06:00:26 -0400

    create basic backend with API endpoints to control the coffeemaker

---
+++ webapp/app/api.py
@@ -0,0 +1,23 @@
+from app import app
+
+from flask import jsonify, request
+
+from app import pin
+
+@app.route('/api/status', methods=['GET'])
+def status():
+    state = pin.status()
+    return jsonify({
+        'brewing': state,
+    })
+
+@app.route('/api/specs', methods=['GET'])
+def specs():
+    return jsonify({'status': 'fail', 'message': 'not implemented'})
+
+@app.route('/api/brew', methods=['POST'])
+def brew():
+    j = request.get_json(force=True)
+    if 'brewing' not in j:
+        abort(400)
+    pin.set_state(j['brewing'])
---
+++ webapp/app/pin.py
@@ -0,0 +1,31 @@
+import os
+
+PIN = 3 # this needs to match the number in init_pin.sh
+
+BASE = '/sys/class/gpio/gpio%d' % (PIN,)
+
+def status():
+    with open(os.path.join(BASE, 'value'), 'rt') as f:
+        data = f.read().strip()
+    if data == '0':
+        return False
+    elif data == '1':
+        return True
+    else:
+        raise RuntimeError('unknown state %s' % data)
+
+def set_state(value):
+    v = '1' if bool(value) else '0'
+    with open(os.path.join(BASE, 'value'), 'wt') as f:
+        f.write(v)
+
+def on():
+    set_state(True)
+
+def off():
+    set_state(False)
+
+def toggle():
+    state = status()
+    set_state(not state)
+    return not state
