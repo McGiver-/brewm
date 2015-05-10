7e08e24df2319623f2b0952758acccf64ef783e5
Brewm Server <brewer@brewm.coffee>
2015-05-10 05:20:47 +0000

    set initial pin state to off

--- init_pin.sh
+++ init_pin.sh
@@ -6,4 +6,5 @@ PIN=17
 
 echo $PIN > /sys/class/gpio/export || true
 echo 'out' > /sys/class/gpio/gpio$PIN/direction
+echo '0' > /sys/class/gpio/gpio$PIN/value
 chown brewer /sys/class/gpio/gpio$PIN/*
